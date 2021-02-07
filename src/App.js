01fb991767d76c1a3345c16d2d397076e36403f5
shrabon-sarker <bikash.webdeveloper5@gmail.com>
2021-02-07 20:09:36 +0600

    create react app and component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import './App.css';
-import logo from './logo.svg';
+
+//imput components
+import Com from './components/index'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          learing react new fit
-        </a>
-      </header>
+      <Com />
     </div>
   );
 }
---
+++ src/components/index.jsx
@@ -0,0 +1,13 @@
+import {Component} from 'react'
+
+class Com extends Component {
+    render(){
+        return(
+            <div>
+                <h2>this is a first react app runing</h2>
+            </div>
+        );
+    }
+}
+
+export default Com;
